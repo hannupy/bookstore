0cb5d0a7c1169e60751280c8281192e98e4a632a
Hannu T. Pysays <hannu@pysays.net>
2026-07-25 17:26:37 +0300

    Final changes before publishing to GitHub pages.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bookstore/', // add this property and the value is the name of your repository
   plugins: [react()],
 })
